24483a0a59c653d7ce3940b60417fd220c1f4133
Ryne <s.ryne@yahoo.com>
2021-10-26 21:09:37 -0500

    -Initial GIT upload -Latest local change migrated DB from SSMS to Postgres

---
+++ Weather.py
@@ -0,0 +1,148 @@
+import os
+from datetime import datetime
+import copy
+import datetime
+import psycopg2
+import pandas as pd
+import dash
+from dash import dcc
+from dash import html
+import dash_bootstrap_components as dbc
+import plotly
+from plotly.subplots import make_subplots
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+
+USER = os.environ.get('DB_USER')
+PASS = os.environ.get('DB_PASS')
+HOST = os.environ.get('DB_HOST')
+PORT = os.environ.get('DB_PORT')
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = html.Div(
+    html.Div([
+        html.H4('Current Weather'),
+        html.Div(id='live-update-text'),
+        dcc.Graph(id='live-update-graph',
+                  figure={'layout': {'height': 850}}),
+        dcc.Interval(
+            id='interval-component',
+            interval=10 * 1000,  # in milliseconds
+            n_intervals=0
+        )
+    ])
+)
+
+
+# Header data updates
+@app.callback(Output('live-update-text', 'children'),
+              Input('interval-component', 'n_intervals'))
+def update_metrics(n):
+    # Connect to SQL database
+    conn = psycopg2.connect(database='Weather',
+                            user=USER,
+                            password=PASS,
+                            host=HOST,
+                            port=PORT)
+
+    # Pull all data, within look back timeframe, into dataframe
+    sql = 'SELECT * FROM weather_data order by weather_data."Time" DESC LIMIT 1'
+    df = pd.read_sql(sql, conn)
+    df['Time'] = pd.to_datetime(df['Time'])
+
+    print('*******Ribbon Refreshed*******')
+
+    style = {'padding': '5px', 'fontSize': '16px'}
+    return [
+        html.Span('Time: ' + str(df['Time'].dt.strftime('%H:%M:%S').values[0]), style=style),
+        html.Span('Status: ' + str(df['Status'].values[0]), style=style),
+        html.Span('Temperature: ' + str(df['Temp'].values[0]) + ' deg F', style=style),
+        html.Span('Humidity: ' + str(df['Humid'].values[0]) + '%', style=style),
+        html.Span('Dew Point: ' + str(round(df['Dew Point'], 2).values[0]) + ' deg F', style=style),
+        html.Span('Rainfall Rate: ' + str(df['Rainfall Rate'].values[0]) + '" per hour', style=style),
+        html.Span('Today' + "'" + 's Rainfall: ' + str(round(float(df['Rainfall']), 2)) + '"', style=style),
+        html.Span('Pressure: ' + str(round(df['Pressure'], 2).values[0]) + ' inHg', style=style)
+    ]
+
+
+# Quad chart update
+@app.callback(Output('live-update-graph', 'figure'),
+              Input('interval-component', 'n_intervals'))
+def update_graph_live(n):
+    # Connect to SQL database
+    conn = psycopg2.connect(database='Weather',
+                            user=USER,
+                            password=PASS,
+                            host=HOST,
+                            port=PORT)
+
+    # Define plot start date
+    chart_cutoff = "'" + (datetime.datetime.now() - pd.Timedelta(days=7)).strftime('%Y/%m/%d %H:%M:%S') + "'"
+
+    # Pull all data, within look back timeframe, into dataframe
+    sql = 'SELECT * FROM weather_data WHERE weather_data."Time" > ' + chart_cutoff + ' ORDER BY weather_data."Time"'
+    df = pd.read_sql(sql, conn)
+    df['Time'] = pd.to_datetime(df['Time'])
+
+    # Create the graph with subplots
+    fig = plotly.subplots.make_subplots(rows=2, cols=2, vertical_spacing=0.1, horizontal_spacing=0.05)
+    fig['layout']['margin'] = {
+        'l': 30, 'r': 10, 'b': 30, 't': 10
+    }
+    fig['layout']['legend'] = {'xanchor': 'left'}
+
+    fig.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)',
+                       'paper_bgcolor': 'rgba(0,0,0,0)'})
+
+    fig.append_trace({
+        'x': df['Time'],
+        'y': df['Temp'],
+        'name': 'Temperature',
+        'mode': 'lines',
+        'type': 'scatter'
+    }, 1, 1)
+    fig.append_trace({
+        'x': df['Time'],
+        'y': df['Dew Point'],
+        'name': 'Dew Point',
+        'mode': 'lines',
+        'type': 'scatter'
+    }, 1, 1)
+    fig.append_trace({
+        'x': df['Time'],
+        'y': df['Humid'],
+        'name': 'Humidity',
+        'mode': 'lines',
+        'type': 'scatter'
+    }, 2, 1)
+    fig.append_trace({
+        'x': df['Time'],
+        'y': df['Rainfall Rate'],
+        'name': 'Rainfall Rate',
+        'mode': 'lines',
+        'type': 'scatter'
+    }, 1, 2)
+    fig.append_trace({
+        'x': df['Time'],
+        'y': df['Rainfall'],
+        'name': 'Rainfall',
+        'mode': 'lines',
+        'type': 'scatter'
+    }, 1, 2)
+    fig.append_trace({
+        'x': df['Time'],
+        'y': df['Pressure'],
+        'name': 'Pressure',
+        'mode': 'lines',
+        'type': 'scatter'
+    }, 2, 2)
+
+    print('*******Chart Refreshed*******')
+
+    return fig
+
+
+if __name__ == '__main__':
+    app.server.run(host='127.0.0.1', port=1080, debug=False, threaded=True)
---
+++ WxDataCollection.py
@@ -0,0 +1,79 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+import requests
+from bs4 import BeautifulSoup
+from pyowm import OWM
+import datetime
+import time
+
+from twilio.base.exceptions import TwilioRestException
+from twilio.rest import Client
+
+# Database credentials
+USER = os.environ.get('DB_USER')
+PASS = os.environ.get('DB_PASS')
+PORT = os.environ.get('DB_PORT')
+
+# Location data
+CITY = os.environ.get('CITY')
+RAIN_SITE = os.environ.get('RAIN_SITE')
+HEADERS = os.environ.get('HEADERS')
+
+# Establish connection with API
+SID = os.environ.get('TWILLIO_SID')
+TOK = os.environ.get('TWILLIO_TOK')
+TO = os.environ.get('TWILLIO_TO')
+FROM = os.environ.get('TWILLIO_FROM')
+KEY = os.environ.get('TWILLIO_API_KEY')
+owm = OWM(KEY)
+mgr = owm.weather_manager()
+
+engine = create_engine('postgresql://' + USER + PASS + '@localhost:' + PORT + '/Weather')
+
+while True:
+    try:
+        obs = mgr.weather_at_place(CITY)
+        data = obs.weather
+
+        # Retrieve weather data points
+        t = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+        temp = data.temperature(unit='fahrenheit')['temp']
+        humid = data.humidity
+        status = data.detailed_status
+        dew_point = temp - (100 - humid) / 5
+        pressure = data.pressure.get('press') * 0.029921
+
+        rain_rate = data.rain.get('1h')
+        if rain_rate is None:
+            rain_rate = 0
+
+        # Scrape rainfall data
+        response = requests.get(RAIN_SITE,
+                                headers={HEADERS})
+        soup = BeautifulSoup(response.text, 'html.parser')
+        rainfall = soup.find('div', id='homeStats').find_all('p')[5].text.split('"')[0]
+
+        df = pd.DataFrame({'Time': [t],
+                           'Temp': [temp],
+                           'Humid': [humid],
+                           'Dew Point': [dew_point],
+                           'Rainfall Rate': [rain_rate],
+                           'Rainfall': [rainfall],
+                           'Pressure': [pressure],
+                           'Status': [status]})
+
+        df.to_sql(name='weather_data', con=engine, if_exists='append')
+        print('Successful Scrape! ' + datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'))
+        time.sleep(10)
+    except:
+        t = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+        print('Warning! ' + str(t))
+        try:
+            client = Client(SID, TOK)
+            client.messages.create(to=TO,
+                                   from_=FROM,
+                                   body="There has been a problem while scraping Wx data at " + str(t))
+        except TwilioRestException as e:
+            print('Text Failed at ' + str(t) + ': ' + str(e))
+            continue
