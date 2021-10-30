2605f95017d756199c67cc79f003f0efb5901048
Ryne <s.ryne@yahoo.com>
2021-10-30 17:44:00 -0500

    -improved visual templates based on wind speed example from Dash Docs -seperated data pull from graph update -add app.css -add style.css -updated requirements.txt

--- Weather.py
+++ Weather.py
@@ -1,6 +1,5 @@
 import os
 from datetime import datetime
-import copy
 import datetime
 import psycopg2
 import pandas as pd
@@ -8,9 +7,6 @@ import dash
 from dash import dcc
 from dash import html
 import dash_bootstrap_components as dbc
-import plotly
-from plotly.subplots import make_subplots
-import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 
 USER = os.environ.get('DB_USER')
@@ -18,131 +14,224 @@ PASS = os.environ.get('DB_PASS')
 HOST = os.environ.get('DB_HOST')
 PORT = os.environ.get('DB_PORT')
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+GRAPH_INTERVAL = os.environ.get("GRAPH_INTERVAL", 1000*10)
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
+                meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}])
+
+app.title = 'Denton Weather'
+
+
+app_color = {"graph_bg": "#082255", "graph_line": "#007ACE"}
+
 app.layout = html.Div(
-    html.Div([
-        html.H4('Current Weather'),
-        html.Div(id='live-update-text'),
-        dcc.Graph(id='live-update-graph',
-                  figure={'layout': {'height': 850}}),
-        dcc.Interval(
-            id='interval-component',
-            interval=10 * 1000,  # in milliseconds
-            n_intervals=0
-        )
-    ])
+    [
+        # header
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.H4("Weather Streaming", className="app__header__title"),
+                        html.P(
+                            "This app continually queries a SQL database and displays live charts of weather data",
+                            className="app__header__title--grey",
+                        ),
+                    ],
+                    className="app__header__desc",
+                ),
+            ],
+            className="app__header",
+        ),
+        html.Div(
+            [
+                # Weather graph
+                html.Div(
+                    [
+                        html.Div(
+                            [html.H6("Temperature (Degree F)", className="graph__title")]
+                        ),
+                        dcc.Graph(
+                            id="temp-graph",
+                            figure=dict(
+                                layout=dict(
+                                    plot_bgcolor=app_color["graph_bg"],
+                                    paper_bgcolor=app_color["graph_bg"],
+                                )
+                            ),
+                        ),
+                        dcc.Interval(
+                            id="interval-component",
+                            interval=int(GRAPH_INTERVAL),
+                            n_intervals=0,
+                        ),
+                    ],
+                    className="two-thirds column temp__container",
+                ),
+                # html.Div(
+                #     [
+                #         # histogram
+                #         html.Div(
+                #             [
+                #                 html.Div(
+                #                     [
+                #                         html.H6(
+                #                             "WIND SPEED HISTOGRAM",
+                #                             className="graph__title",
+                #                         )
+                #                     ]
+                #                 ),
+                #                 html.Div(
+                #                     [
+                #                         dcc.Slider(
+                #                             id="bin-slider",
+                #                             min=1,
+                #                             max=60,
+                #                             step=1,
+                #                             value=20,
+                #                             updatemode="drag",
+                #                             marks={
+                #                                 20: {"label": "20"},
+                #                                 40: {"label": "40"},
+                #                                 60: {"label": "60"},
+                #                             },
+                #                         )
+                #                     ],
+                #                     className="slider",
+                #                 ),
+                #                 html.Div(
+                #                     [
+                #                         dcc.Checklist(
+                #                             id="bin-auto",
+                #                             options=[
+                #                                 {"label": "Auto", "value": "Auto"}
+                #                             ],
+                #                             value=["Auto"],
+                #                             inputClassName="auto__checkbox",
+                #                             labelClassName="auto__label",
+                #                         ),
+                #                         html.P(
+                #                             "# of Bins: Auto",
+                #                             id="bin-size",
+                #                             className="auto__p",
+                #                         ),
+                #                     ],
+                #                     className="auto__container",
+                #                 ),
+                #                 dcc.Graph(
+                #                     id="wind-histogram",
+                #                     figure=dict(
+                #                         layout=dict(
+                #                             plot_bgcolor=app_color["graph_bg"],
+                #                             paper_bgcolor=app_color["graph_bg"],
+                #                         )
+                #                     ),
+                #                 ),
+                #             ],
+                #             className="graph__container first",
+                #         ),
+                #         # wind direction
+                #         html.Div(
+                #             [
+                #                 html.Div(
+                #                     [
+                #                         html.H6(
+                #                             "WIND DIRECTION", className="graph__title"
+                #                         )
+                #                     ]
+                #                 ),
+                #                 dcc.Graph(
+                #                     id="wind-direction",
+                #                     figure=dict(
+                #                         layout=dict(
+                #                             plot_bgcolor=app_color["graph_bg"],
+                #                             paper_bgcolor=app_color["graph_bg"],
+                #                         )
+                #                     ),
+                #                 ),
+                #             ],
+                #             className="graph__container second",
+                #         ),
+                #     ],
+                #     className="one-third column histogram__direction",
+                # ),
+            ],
+            className="app__content",
+        ),
+    ],
+    className="app__container",
 )
 
 
-# Header data updates
-@app.callback(Output('live-update-text', 'children'),
-              Input('interval-component', 'n_intervals'))
-def update_metrics(n):
+def data_pull():
     # Connect to SQL database
-    conn = psycopg2.connect(database='Weather',
-                            user=USER,
-                            password=PASS,
-                            host=HOST,
-                            port=PORT)
-
-    # Pull all data, within look back timeframe, into dataframe
-    sql = 'SELECT * FROM weather_data order by weather_data."Time" DESC LIMIT 1'
-    df = pd.read_sql(sql, conn)
-    df['Time'] = pd.to_datetime(df['Time'])
-
-    print('*******Ribbon Refreshed*******')
-
-    style = {'padding': '5px', 'fontSize': '16px'}
-    return [
-        html.Span('Time: ' + str(df['Time'].dt.strftime('%H:%M:%S').values[0]), style=style),
-        html.Span('Status: ' + str(df['Status'].values[0]), style=style),
-        html.Span('Temperature: ' + str(df['Temp'].values[0]) + ' deg F', style=style),
-        html.Span('Humidity: ' + str(df['Humid'].values[0]) + '%', style=style),
-        html.Span('Dew Point: ' + str(round(df['Dew Point'], 2).values[0]) + ' deg F', style=style),
-        html.Span('Rainfall Rate: ' + str(df['Rainfall Rate'].values[0]) + '" per hour', style=style),
-        html.Span('Today' + "'" + 's Rainfall: ' + str(round(float(df['Rainfall']), 2)) + '"', style=style),
-        html.Span('Pressure: ' + str(round(df['Pressure'], 2).values[0]) + ' inHg', style=style)
-    ]
-
-
-# Quad chart update
-@app.callback(Output('live-update-graph', 'figure'),
-              Input('interval-component', 'n_intervals'))
-def update_graph_live(n):
-    # Connect to SQL database
-    conn = psycopg2.connect(database='Weather',
-                            user=USER,
-                            password=PASS,
-                            host=HOST,
-                            port=PORT)
+    conn = psycopg2.connect(database='Weather', user=USER, password=PASS, host=HOST, port=PORT)
 
     # Define plot start date
     chart_cutoff = "'" + (datetime.datetime.now() - pd.Timedelta(days=7)).strftime('%Y/%m/%d %H:%M:%S') + "'"
 
     # Pull all data, within look back timeframe, into dataframe
-    sql = 'SELECT * FROM weather_data WHERE weather_data."Time" > ' + chart_cutoff + ' ORDER BY weather_data."Time"'
+    sql = 'SELECT "Time", "Temp" FROM weather_data WHERE weather_data."Time" > ' + chart_cutoff + ' ORDER BY weather_data."Time"'
     df = pd.read_sql(sql, conn)
     df['Time'] = pd.to_datetime(df['Time'])
+    return df
 
-    # Create the graph with subplots
-    fig = plotly.subplots.make_subplots(rows=2, cols=2, vertical_spacing=0.1, horizontal_spacing=0.05)
-    fig['layout']['margin'] = {
-        'l': 30, 'r': 10, 'b': 30, 't': 10
-    }
-    fig['layout']['legend'] = {'xanchor': 'left'}
 
-    fig.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)',
-                       'paper_bgcolor': 'rgba(0,0,0,0)'})
+# # Header data updates
+# @app.callback(Output('live-update-text', 'children'),
+#               Input('interval-component', 'n_intervals'))
+# def update_metrics(n):
+#     weather = data_pull()
+#
+#     time = 'Time: ' + str(weather['Time'].dt.strftime('%H:%M:%S').values[0])
+#     status = 'Status: ' + str(weather['Status'].values[0])
+#     temp = 'Temperature: ' + str(weather['Temp'].values[0]) + ' deg F'
+#     humid = 'Humidity: ' + str(weather['Humid'].values[0]) + '%'
+#     dew = 'Dew Point: ' + str(round(weather['Dew Point'], 2).values[0]) + ' deg F'
+#     rain_rate = 'Rainfall Rate: ' + str(weather['Rainfall Rate'].values[0]) + '" per hour'
+#     rain = 'Today' + "'" + 's Rainfall: ' + str(round(float(weather['Rainfall']), 2)) + '"'
+#     press = 'Pressure: ' + str(round(weather['Pressure'], 2).values[0]) + ' inHg'
+#
+#     return time
 
-    fig.append_trace({
-        'x': df['Time'],
-        'y': df['Temp'],
-        'name': 'Temperature',
-        'mode': 'lines',
-        'type': 'scatter'
-    }, 1, 1)
-    fig.append_trace({
-        'x': df['Time'],
-        'y': df['Dew Point'],
-        'name': 'Dew Point',
-        'mode': 'lines',
-        'type': 'scatter'
-    }, 1, 1)
-    fig.append_trace({
-        'x': df['Time'],
-        'y': df['Humid'],
-        'name': 'Humidity',
-        'mode': 'lines',
-        'type': 'scatter'
-    }, 2, 1)
-    fig.append_trace({
-        'x': df['Time'],
-        'y': df['Rainfall Rate'],
-        'name': 'Rainfall Rate',
-        'mode': 'lines',
-        'type': 'scatter'
-    }, 1, 2)
-    fig.append_trace({
-        'x': df['Time'],
-        'y': df['Rainfall'],
-        'name': 'Rainfall',
-        'mode': 'lines',
-        'type': 'scatter'
-    }, 1, 2)
-    fig.append_trace({
-        'x': df['Time'],
-        'y': df['Pressure'],
-        'name': 'Pressure',
-        'mode': 'lines',
-        'type': 'scatter'
-    }, 2, 2)
 
-    print('*******Chart Refreshed*******')
+# Quad chart update
+@app.callback(Output('temp-graph', 'figure'),
+              Input('interval-component', 'n_intervals'))
+def update_graph_live(n):
+    weather = data_pull()
 
-    return fig
+    trace = dict(
+        type="scatter",
+        x=weather['Time'],
+        y=weather["Temp"],
+        line={"color": "#42C4F7"},
+        hoverinfo="skip",
+        mode="lines",
+    )
+
+    layout = dict(
+        plot_bgcolor=app_color["graph_bg"],
+        paper_bgcolor=app_color["graph_bg"],
+        font={"color": "#fff"},
+        height=700,
+        xaxis={
+            "showline": True,
+            "zeroline": False,
+            "fixedrange": True,
+            "title": "Date",
+        },
+        yaxis={
+            "showgrid": True,
+            "showline": True,
+            "fixedrange": True,
+            "zeroline": False,
+            "gridcolor": app_color["graph_line"],
+            'title': 'Temperature',
+        },
+    )
+
+    return dict(data=[trace], layout=layout)
 
 
 if __name__ == '__main__':
-    app.server.run(debug=False, threaded=True)
+    app.run_server(debug=True, threaded=True)
--- WxDataCollection.py
+++ WxDataCollection.py
@@ -18,18 +18,17 @@ PORT = os.environ.get('DB_PORT')
 # Location data
 CITY = os.environ.get('CITY')
 RAIN_SITE = os.environ.get('RAIN_SITE')
-HEADERS = os.environ.get('HEADERS')
 
 # Establish connection with API
-SID = os.environ.get('TWILLIO_SID')
-TOK = os.environ.get('TWILLIO_TOK')
-TO = os.environ.get('TWILLIO_TO')
-FROM = os.environ.get('TWILLIO_FROM')
-KEY = os.environ.get('TWILLIO_API_KEY')
+# SID = os.environ.get('TWILLIO_SID')
+# TOK = os.environ.get('TWILLIO_TOK')
+# TO = os.environ.get('TWILLIO_TO')
+# FROM = os.environ.get('TWILLIO_FROM')
+KEY = os.environ.get('OWM_API_KEY')
 owm = OWM(KEY)
 mgr = owm.weather_manager()
 
-engine = create_engine('postgresql://' + USER + PASS + '@localhost:' + PORT + '/Weather')
+engine = create_engine('postgresql://' + USER + ':' + PASS + '@localhost:' + PORT + '/Weather')
 
 while True:
     try:
@@ -50,7 +49,9 @@ while True:
 
         # Scrape rainfall data
         response = requests.get(RAIN_SITE,
-                                headers={HEADERS})
+                                headers={"User-Agent": "Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) "
+                                                       "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 "
+                                                       "Mobile Safari/537.36"})
         soup = BeautifulSoup(response.text, 'html.parser')
         rainfall = soup.find('div', id='homeStats').find_all('p')[5].text.split('"')[0]
 
@@ -69,11 +70,11 @@ while True:
     except:
         t = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
         print('Warning! ' + str(t))
-        try:
-            client = Client(SID, TOK)
-            client.messages.create(to=TO,
-                                   from_=FROM,
-                                   body="There has been a problem while scraping Wx data at " + str(t))
-        except TwilioRestException as e:
-            print('Text Failed at ' + str(t) + ': ' + str(e))
-            continue
+        # try:
+        #     client = Client(SID, TOK)
+        #     client.messages.create(to=TO,
+        #                            from_=FROM,
+        #                            body="There has been a problem while scraping Wx data at " + str(t))
+        # except TwilioRestException as e:
+        #     print('Text Failed at ' + str(t) + ': ' + str(e))
+        #     continue
